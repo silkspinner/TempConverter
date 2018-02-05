5a80159b0f1673d9d7160574899841be037415c1
Ron Nims <rleenims@gmail.com>
2018-02-05 15:17:58 -0800

    Adds Java logic to implement temperature conversion

--- app/src/main/java/com/nimsdev/tempconverter/MainActivity.java
+++ app/src/main/java/com/nimsdev/tempconverter/MainActivity.java
@@ -2,12 +2,52 @@ package com.nimsdev.tempconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity implements TextView.OnEditorActionListener  {
+
+    // create class properties to hold widget references
+    private EditText fahrenheitEditText;
+    private TextView celsiusTextView;
+
+    private String fahrenheitInputString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // establish widget references
+        fahrenheitEditText = findViewById(R.id.fahrenheitEditText);
+        celsiusTextView = findViewById(R.id.celsiusTextView);
+
+        fahrenheitEditText.setOnEditorActionListener(this);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+        calculateAndDisplay();
+        return true;
+    }
+
+    private void calculateAndDisplay() {
+        //gather fahrenheit degrees input
+        fahrenheitInputString = fahrenheitEditText.getText().toString();
+        float fahrenheitInput;
+        if (fahrenheitInputString.equals("")) {
+            fahrenheitInput = 0;
+        } else {
+            fahrenheitInput = Float.parseFloat(fahrenheitInputString);
+        }
+
+        // calculate output dergrees in celsius
+        float celsiusOutput = (fahrenheitInput - 32) * 5/9;
+
+        NumberFormat degrees = NumberFormat.getIntegerInstance();
+        celsiusTextView.setText(degrees.format(celsiusOutput));
     }
 }
